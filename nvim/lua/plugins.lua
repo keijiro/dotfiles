42cd992387c796a35ec1ba30291b92dda941522a
Keijiro Takahashi <keijiro@gmail.com>
2024-08-19 09:42:13 +0900

    nvim: Markdown Preview

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -98,6 +98,15 @@ return {
   },
   -- }}}
 
+  -- Markdown Preview {{{
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function() vim.fn["mkdp#util#install"]() end
+  },
+  -- }}}
+
   -- Theme: Catppuccin {{{
   {
     "catppuccin/nvim",
